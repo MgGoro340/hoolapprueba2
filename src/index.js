042cdfb9f9488999c6bc97293d9cbb6f988618cd
mggoro <marting@epidataconsulting.com>
2020-10-24 16:25:50 -0300

    fdsfsdfsdf

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CounterApp } from './componentes/CounterApp';
-import { CounterWithCustomHook } from './componentes/CounterWithCustomHook';
-import { MultipleCustomHooks } from './componentes/examples/MultipleCustomHooks';
-import { FormWithCustomHook } from './componentes/useEffect/FormWithCustomHook';
-import { FocusScreen } from './componentes/useRef/FocusScreen';
-import { RealExampleUseRef } from './componentes/useRef/RealExampleUseRef';
-import { HooksApp } from './HooksApp';
+
 
 
 
